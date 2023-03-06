ea98a79f2bfb87beb144a9633264c245027e78ff
JVSB1 <124447168+JVSB1@users.noreply.github.com>
2023-03-06 19:28:40 -0300

    Fix Unit Test

--- test.py
+++ test.py
@@ -16,5 +16,5 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "JVSB1")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello")
 
